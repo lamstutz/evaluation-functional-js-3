0d09a0251b40fb2a48dafa468a78df299a15c292
lamstutz <louis.amstutz@gmail.com>
2018-03-02 10:46:58 +0100

    exo 4 part 1

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,6 +1,25 @@
+
 const countSequence = (sequence, input) => {
-  // Your future job begins here ...
+  if (input) {
+    return countSeq(sequence, input);
+  } else {
+    return (input) => {
+      return countSeq(sequence, input);
+    };
+  }
 };
+
+const countSeq = (sequence, input) => {
+  console.log(sequence.length);
+  console.log(input.length);
+  if (!sequence || !input || sequence.length === 0 || input.length === 0) {
+    return 0;
+  } else {
+    return input.split(sequence).length - 1;
+  }
+};
+
+
 
 module.exports = {
   title: "Exercise 4",
