f1222a3253845bca7b6da5a906eb1b0b1c92b7e7
lamstutz <louis.amstutz@gmail.com>
2018-03-02 09:44:04 +0100

    Exercise 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,10 +1,28 @@
-const moment = require("moment");
+const moment = require('moment');
 
-const sortProduct = input => {
-  // Your future job begins here ...
+const sortProduct = products => {
+  return products.filter(productIsAfter2000).sort(sortByProductDate).reduce(formatProduct, {});
 };
 
+const productIsAfter2000 = (product) => moment(product.dateAdded).isAfter('2000-01-01', 'year');
+
+
+const formatProduct = (acc, product) => {
+  if (!acc[product.name]) {
+    acc[product.name] = [];
+  }
+  acc[product.name].push({
+    name: product.name,
+    year: moment(product.dateAdded).year(),
+    monthOfYear: moment(product.dateAdded).month() + 1,
+    quantity: product.quantity
+  });
+  return acc;
+};
+
+const sortByProductDate = (product1, product2) => product1.dateAdded - product2.dateAdded;
+
 module.exports = {
-  title: "Exercise 1",
+  title: 'Exercise 1',
   run: sortProduct
 };
